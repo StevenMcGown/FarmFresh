14482c6b758b8b758db9eaf0e40fb13923d0a6f1
StevenMcGown <stevenmcgown1@gmail.com>
2021-04-12 19:16:52 -0400

    made lots of changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'signUp',component:SignUpComponent},
+  {path:'signIn',component:SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
